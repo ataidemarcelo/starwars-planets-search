629afb57037989b0548bfb287cd44507b24162be
Marcelo Ataíde <msa.trybe@gmail.com>
2022-09-29 12:24:26 -0300

    Requisito 01 -Faça uma requisição para o endpoint `/planets` da API de Star Wars e preencha uma tabela com os dados retornados.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    const getPlanets = async () => {
+      const response = await fetch('https://swapi-trybe.herokuapp.com/api/planets');
+      const result = await response.json();
+      const planetsResult = result.results;
+      setPlanets(planetsResult);
+    };
+
+    getPlanets();
+  }, []);
+
   return (
-    <span>Hello, App!!!</span>
+    <>
+      <div>
+        <h1>Projeto Star Wars - Trybe</h1>
+        <input type="text" />
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {planets.map((planet) => (
+            <tr
+              key={ planet.name }
+            >
+              <td>
+                { planet.name }
+              </td>
+              <td>{ planet.rotation_period }</td>
+              <td>{ planet.orbital_period }</td>
+              <td>{ planet.diameter }</td>
+              <td>{ planet.climate }</td>
+              <td>{ planet.gravity }</td>
+              <td>{ planet.terrain }</td>
+              <td>{ planet.surface_water }</td>
+              <td>{ planet.population }</td>
+              <td>
+                {planet.films.map((url) => (
+                  <span key={ url }>
+                    <a href={ url }>{url }</a>
+                  </span>
+                ))}
+              </td>
+              <td>{ planet.created }</td>
+              <td>{ planet.edited }</td>
+              <td>
+                <a href={ planet.url }>{ planet.url }</a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
 
